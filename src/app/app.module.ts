f022714dd7d40b7823670fe0988c333bcb39208f
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-16 14:34:46 -0300

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/persons/person-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+
+export class PersonInputComponent {
+  enteredPersonName: string = '';
+
+  onCreatePerson() {
+    console.log("created a person:", this.enteredPersonName);
+    this.enteredPersonName = '';
+  }
+}
