74f20b6df67da335427cf013649232faa890effa
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-16 14:44:25 -0300

    two way binding and events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   persons = ['Caitlin', 'Nicole', 'Jessica', 'Meghan'];
+
+  onPersonCreated(name: string) {
+    this.persons.push(name);
+  }
 }
--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-person-input',
@@ -8,9 +8,11 @@ import { Component } from '@angular/core';
 
 export class PersonInputComponent {
   enteredPersonName: string = '';
+  @Output() personCreate = new EventEmitter<string>();
 
   onCreatePerson() {
     console.log("created a person:", this.enteredPersonName);
+    this.personCreate.emit(this.enteredPersonName)
     this.enteredPersonName = '';
   }
 }
